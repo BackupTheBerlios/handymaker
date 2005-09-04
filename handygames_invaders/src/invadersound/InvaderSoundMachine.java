edc04c2d355b9076165ef5884e36b12fea66848c
redrum <redrum>
2005-09-04 15:21:59 +0000

    *** empty log message ***

--- handygames_invaders/src/invadersound/InvaderSoundMachine.java
+++ handygames_invaders/src/invadersound/InvaderSoundMachine.java
@@ -1,31 +1,13 @@
 package invadersound;
 
-import javax.microedition.media.MediaException;
-import javax.microedition.media.Player;
-
 import sound.SoundMachine;
-import gameworld.Entity;
 import gamemachine.*;
-import gamemachine.level.Level;
+
 
 public class InvaderSoundMachine extends SoundMachine {
 	
-	private GameMachine gm = null;
-	private Level level =null;
-	
-    public InvaderSoundMachine (GameMachine g) {
-    	gm =g; 
-    }
-	public void playfromResource (Entity e) {
-		//int soundSetID = e.getSoundSet();
-		//int soundID    = e.getSoundNumber();
-	    level = gm.getCurrLevel();
-		Player [][] player = level.getSounds(e.getSoundSet());
-		try {
-			player [e.getSoundNumber()][0].start();
-		} catch (MediaException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+	public InvaderSoundMachine (GameMachine g) {
+		super (g);
 	}
+	 
 }
