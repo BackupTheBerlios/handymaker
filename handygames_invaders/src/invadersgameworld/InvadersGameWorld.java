51cda759bb2d8bec731919ea352ea28a05652231
sodium <sodium>
2005-09-08 14:00:35 +0000

    *** empty log message ***

--- handygames_invaders/src/invadersgameworld/InvadersGameWorld.java
+++ handygames_invaders/src/invadersgameworld/InvadersGameWorld.java
@@ -11,10 +11,17 @@ import gameworld.*;
 public class InvadersGameWorld extends GameWorld
 {
 	int m_Level;
+	long m_LastUpdate;
+	TankEntity m_Tank;
 	
 	public static final int ENTITY_PLAYER=1;
 	public static final int ENTITY_SHOT1=2;
 	public static final int ENTITY_INVADER1=3;
+	
+	public static final long FRAMEDELAY=50;
+	
+	public static final int TANK_STEP=2; 
+	public static final int BORDER_RIGHT=172;
 	
 	public InvadersGameWorld(InvaderGameMachine invadersGameMachine)
 	{
@@ -26,7 +33,7 @@ public class InvadersGameWorld extends GameWorld
 	{
 		/* diesen block hinzugefügt + konstruktor geändert*/
 		/* ANFANG */
-		Level l = m_GameMachine.getCurrLevel();
+		Level l = gameMachine.getCurrLevel();
 
 		int invaderCount = l.getLevelIntValue(InvadersSettings.INVADERCOUNT);
 		
@@ -50,20 +57,6 @@ public class InvadersGameWorld extends GameWorld
 		
 		
 		se=new SimpleEntity(ENTITY_INVADER1,ENTITY_INVADER1,0,0,30,30);
-		switch(m_Level) // level daten aus script oä rauslesen !!!
-		{
-			case 1:
-				for(int i=0;i<6;++i)
-				{
-					for(int j=0;j<4;++j)
-					{
-						se.setPosition(40*i+20,40*j+20);
-						addSimpleEntity(new SimpleEntity(se));
-					}
-				}
-			break;
-		
-		}
 		
 		//player ship
 		
@@ -72,12 +65,33 @@ public class InvadersGameWorld extends GameWorld
 	
 	public int init()
 	{
+		m_LastUpdate=currentTimeMillis();
 		loadNextLevel();
 		return 0;
 	}
 	
 	public int update()
 	{
+		long t=currentTimeMillis();
+		
+		while(m_LastUpdate+FRAMEDELAY<t)
+		{
+			//move invaders
+			//move player
+			switch(m_TankEntity.getMovestatus())
+			{
+				case TankEntity.MOVE_LEFT:
+					if (m_Tank.getX()>=TANK_STEP) m_Tank.move(-TANK_STEP,0);
+				break;
+				
+				case TankEntity.MOVE_RIGHT:
+					if (m_Tank.getX()<=BORDER_RIGHT-TANK_STEP) m_Tank.move(TANK_STEP,0);
+				break;
+			}
+			//move/create shots
+			
+			m_LastUpdate+=FRAMEDELAY;
+		}
 		return 0;
 	}
 	
@@ -86,7 +100,17 @@ public class InvadersGameWorld extends GameWorld
 		return 0;
 	}
 	
-//	public int command(int p_command)
-//	{
-//	}
+	public int command(int p_Command)
+	{
+		if (m_Tank==null) return -1;
+		switch (p_Command)
+		{
+			case 1:m_Tank.setShotstatus(TankEntity.SHOT_LEFT);break;
+			case 2:m_Tank.setShotstatus(TankEntity.SHOT_STRAIGHT);break;
+			case 3:m_Tank.setShotstatus(TankEntity.SHOT_RIGHT);break;
+			case 4:m_Tank.setMovestatus(TankEntity.MOVE_LEFT);break;
+			case 5:m_Tank.setMovestatus(TankEntity.MOVE_NONE);break;
+			case 6:m_Tank.setMovestatus(TankEntity.MOVE_RIGHT);break;
+		}
+	}
 }
